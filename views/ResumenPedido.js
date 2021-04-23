ead68568e03bf4b4ec4c8cdd52f3e6980e737bd9
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-22 21:00:40 -0600

    Vistas de la APP

---
+++ views/ResumenPedido.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+
+const ResumenPedido = () => {
+    return (
+        <Text>Resumen pedido</Text>
+    )
+}
+
+export default ResumenPedido
+
