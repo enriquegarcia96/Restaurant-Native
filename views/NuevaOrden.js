28db4a94d7e7cdf55426bb988cb6bed81d41f124
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-23 21:43:08 -0600

    navegando al menu de los platillos

--- views/NuevaOrden.js
+++ views/NuevaOrden.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import { Text } from 'react-native'
+import { View, StyleSheet } from 'react-native';
+import { Container, Button, Text } from 'native-base';
+import { useNavigation } from '@react-navigation/native'
+
+
+import globalStyles from '../styles/global';
 
 const NuevaOrden = () => {
+
+    const navigation = useNavigation()
+
     return (
 
-        <Text>NuevoOrden</Text>
+        <Container style={globalStyles.contenedor} >
+            <View style={[globalStyles.contenido, styles.contenido]}>
+                <Button
+                    style={globalStyles.boton}
+                    rounded
+                    block
+                    onPress={ () => navigation.navigate('Menu') }
+                >
+                    <Text style={globalStyles.botonTexto} >Crear Nueva Orden</Text>
+                </Button>
+            </View>
+        </Container>
     
     )
 }
+
+
+const styles = StyleSheet.create({
+
+    contenido:{
+        flexDirection: 'column',
+        justifyContent: 'center'
+    }
+
+})
 
 export default NuevaOrden
