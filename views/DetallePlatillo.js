4b67568b4eeb75abc42681d77b0efe98f85b1e52
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-24 12:17:59 -0600

    Mostrando el producto seleccionado

--- views/DetallePlatillo.js
+++ views/DetallePlatillo.js
@@ -1,10 +1,44 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useContext } from 'react';
+import { Image } from 'react-native';
+
+import PedidosContext from '../context/pedidos/pedidosContext';
+import {
+    Container,
+    Content,
+    Footer, 
+    FooterTab,
+    Button,
+    Body,
+    Text,
+    H1,
+    Card,
+    CardItem
+} from 'native-base';
+import globalStyles from '../styles/global';
 
 
 const DetallePlatillo = () => {
+
+    //--- Pedido Context ---//
+    const { platillo }  = useContext(PedidosContext);
+    const { nombre, imagen, descripcion, precio } = platillo;
+    
+
     return (
-        <Text>DetallePlatillo</Text>
+        <Container style={ globalStyles.contenedor } >
+            <Content style={ globalStyles.contenido } >
+                <H1 style={ globalStyles.titulo } >{nombre}</H1>
+                <Card>
+                    <CardItem>
+                        <Body>
+                            <Image style={ globalStyles.imagen } source={{ uri: imagen }} />
+                        </Body>
+                    </CardItem>
+                </Card>
+
+            </Content>
+
+        </Container>
     )
 }
 
