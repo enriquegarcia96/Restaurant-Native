de603f38575f1f2148449d8181164202e681617a
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-25 17:01:49 -0600

    Creando las funciones y los types para pasarlos a los demas componentes de la APP

--- context/pedidos/pedidosState.js
+++ context/pedidos/pedidosState.js
@@ -3,7 +3,13 @@ import React, { useReducer } from 'react';
 import PedidosReducer from './pedidosReducer' ;
 import PedidosContext from './pedidosContext';
 
-import { SELECCIONAR_PRODUCTO } from '../../types';
+import { 
+    SELECCIONAR_PRODUCTO, 
+    CONFIRMAR_ORDENAR_PLATILLO, 
+    MOSTRAR_RESUMEN, 
+    ELIMINAR_PRODUCTO, 
+    PEDIDO_ORDENADO
+} from '../../types';
 
 
 const PedidosState = (props) => {
@@ -12,7 +18,9 @@ const PedidosState = (props) => {
     //--- crear state Inicial ---//
     const initialState  = {
         pedido: [], // puede ordenar multiples pedidos el cliente
-        platillo: null
+        platillo: null,
+        total: 0,
+        idpedido: ''
     }
 
 
@@ -27,16 +35,56 @@ const PedidosState = (props) => {
             type: SELECCIONAR_PRODUCTO,
             payload: platillo, //payload: es lo que cambia el state
         })
+    }
 
+    //--- Cuando el usuario confirma un platillo ---//
+    const guardarPedido = (pedido) =>{
+        dispatch({
+            type: CONFIRMAR_ORDENAR_PLATILLO,
+            payload: pedido
+        })
+    }
+
+    //--- Mustra el total a Pagar en el resumen ---//
+    const mostrarResumen = total =>{
+
+        dispatch({
+            type: MOSTRAR_RESUMEN,
+            payload: total
+
+        })
+    }
+
+    //--- Elimina un articulo del carrito --//
+    const eliminarProducto = (id) => {
+
+        dispatch({
+            type: ELIMINAR_PRODUCTO,
+            payload: id
+        })
+    }
+
+    const pedidoRealizado = (id) => {
+
+        dispatch({
+            type: PEDIDO_ORDENADO,
+            payload: id
+        })
     }
 
 
     return(
         <PedidosContext.Provider
             value={{//para debugiar
-                pedido: state.pedido, //paso este state a los demas componentes
+                pedido: state.pedido, //paso lo state a los demas componentes
                 platillo: state.platillo,
-                seleccionarPlatillo,
+                total: state.total,
+                idpedido: state.idpedido,
+                seleccionarPlatillo,// funciones para que los demas componentes los utilicen
+                guardarPedido, 
+                mostrarResumen, 
+                eliminarProducto,
+                pedidoRealizado
             }}
         >
             {props.children}
